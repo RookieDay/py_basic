4da9ba9360d689f3606925fbab87214f356ff6ba
Rookieday <1109318841@qq.com>
2017-12-30 23:31:40 +0800

    add

--- pydata-notebook/README.py
+++ pydata-notebook/README.py
@@ -459,4 +459,57 @@ frame.loc[['a', 'b', 'c', 'd'], states]
 # df1.reindex(columns=df2.columns, fill_value=0)  reindex（重建索引）的时候，也可以使用fill_value
 # 1 / df1  -- > 逆运算  df1.rdiv(1)
 
+# frame = pd.DataFrame(np.random.randn(4, 3), columns=list('bde'),
+#                      index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+# 把一个用在一维数组上的函数，应用在一行或一列上。要用到DataFrame中的apply函数
+# f = lambda x: x.max() - x.min()
+# frame.apply(f)
+# frame.apply(f, axis=1)
+
+# def f(x):
+#     return pd.Series([x.min(), x.max()], index=['min', 'max'])
+# frame.apply(f,axis=1)
+
+# element-wise的python函数也能用。假设想要格式化frame中的浮点数，变为string。可以用apply map
+# series有一个map函数，能用来实现element-wise函数
+# format = lambda x: '%.2f' % x
+# frame.applymap(format)
+# frame['e'].map(format)
+
+
+# 按row或column index来排序的话，可以用sort_index方法，会返回一个新的object
+obj = pd.Series(range(4), index=['d', 'a', 'b', 'c'])
+obj.sort_index()
+
+# 在DataFrame，可以用index或其他axis来排序
+# frame = pd.DataFrame(np.arange(8).reshape((2, 4)),
+#                      index=['three', 'one'],
+#                      columns=['d', 'a', 'b', 'c'])
+# frame
+# frame.sort_index(axis=1)
+# 默认是升序，可以设置降序
+# frame.sort_index(axis=1, ascending=False)
+
+# 通过值来排序，用sort_values方法,缺失值会被排在最后
+obj = pd.Series([4, np.nan, 7, np.nan, -3, 2])
+obj.sort_values()
+
+
+# 对于一个DataFrame，可以用一列或多列作为sort keys。这样的话，只需要把一列多多列的名字导入到sort_values即可,多列排序的话，传入一个list of names
+frame = pd.DataFrame({'b': [4, 7, -3, 2], 'a': [0, 1, 0, 1]})
+frame.sort_values(by='b')
+frame.sort_values(by=['a', 'b'])
+# ranking（排名）是给有效的数据分配数字。rank方法能用于series和DataFrame，rank方法默认会给每个group一个mean rank（平均排名）。rank 表示在这个数在原来的Series中排第几名，有相同的数，取其排名平均（默认）作为值
+obj = pd.Series([7, -5, 7, 4, 2, 0, 4])
+obj.rank()
+# rank也可以根据数据被观测到的顺序来设定
+# obj.rank(method='first')
+
+# index的is_unique特性能告诉我们label是否是唯一的
+obj = pd.Series(range(5), index=['a', 'a', 'b', 'b', 'c'])
+obj.index.is_unique
+# 数据选择对于重复label则表现有点不同。如果一个label有多个值，那么就会返回一个series, 如果是label只对应一个值的话，会返回一个标量
+df = pd.DataFrame(np.random.randn(4, 3), index=['a', 'a', 'b', 'b'])
+df.loc['b']
+
 
